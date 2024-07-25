954b795c3803923de9cbb274ee8ee70b145a1456
joshbodunrin <83253910+joshbodunrin@users.noreply.github.com>
2024-07-24 21:03:44 -0400

    added main(no reason) and stock class

---
+++ stock.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <string>
+
+class Stock {
+
+    private:
+
+    string name;
+    float currentValue;
+    
+    Stock();
+
+    ~Stock();
+
+    void setName() {
+        this.name = NULL;
+    }
+
+    void setCurrentValue() {
+
+    }
+
+    string getName() {
+
+    }
+
+    string getCurrentValue() {
+        
+    }
+}

0148f293ee3fb9968a6803f3089e57fc779379d1
joshbodunrin <83253910+joshbodunrin@users.noreply.github.com>
2024-08-01 20:05:51 -0400

    woring on making proper application

--- main.cpp
+++ main.cpp
@@ -1,18 +1,12 @@
+#include "main.h"
 #include "stock.h"
 #include "order.h"
 
 
 #include <iostream>
 
-int main() {
-
-    //Stock newStock = Stock("IBM");
-
-    //Order newOrder = Order(&newStock, 2.0);
-
-    int option;
-
-    std::cout << "Welcome to Trading Platform Simulator!" << std::endl << std::endl << std::endl; 
+void optionMenu() {
+    char option;
     while(1) {
         std::cout << "Choose Option:" << std:: endl;
         std::cout << "1. Look up Stock " << std::endl;
@@ -20,9 +14,46 @@ int main() {
         std::cout << "3. View Portfolio " << std:: endl;
         std::cout << "4. View Order History " << std:: endl;
         std::cout << "5. Exit " << std:: endl;
-        std::cin >> option;
-        if (option == 5) break;
+        std::cin >> option; 
+        std:: cout << std::endl;
+        switch (option) {
+            case '1':
+                lookUpStock();
+                break;
+            case '2':
+                //View Balance
+                break;
+            case '3':
+                //viewPortfolio()
+                break;
+            case '4':
+                //viewOrderHistory();
+                break;
+            case '5':
+                break;
+        }
     }
+}
+
+void lookUpStock() {
+    std::string symbol;
+    std::cout << std::endl << std::endl;
+    std::cout << "Enter Symbol you want to see data for: ";
+    std::cin >> symbol;
+    Stock lookUp = Stock(symbol);
+    std::cout << "Current Price: " << lookUp.getPrice() << std::endl;
+    //optionMenu();
+}
+
+int main() {
+
+    //Stock newStock = Stock("IBM");
+
+    //Order newOrder = Order(&newStock, 2.0);
+
+    std::cout << "Welcome to Trading Platform Simulator!" << std::endl << std::endl << std::endl; 
+    optionMenu();
+
 
 
 }
